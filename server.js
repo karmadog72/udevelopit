497024690a9a72a46ad908320009fca0c1f51367
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-11-27 16:04:46 -0700

    added params

--- server.js
+++ server.js
@@ -24,10 +24,36 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
-});
+// // GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
 
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//               VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
